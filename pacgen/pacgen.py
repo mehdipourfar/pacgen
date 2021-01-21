e7682f773277458116997a9daa666ac91b080619
Mehdi Pourfar <mehdipourfar@gmail.com>
2021-01-21 04:31:51 +0330

    refactor

--- pacgen/pacgen.py
+++ pacgen/pacgen.py
@@ -4,52 +4,7 @@ from urllib.parse import urlparse
 import yaml
 
 
-class PacGen:
-    def __init__(self, config_path):
-        with open(config_path, 'r') as f:
-            data = yaml.load(f, Loader=yaml.FullLoader)
-
-        self.proxies = {
-            k: self.parse_proxy(v) for k, v in data['proxies'].items()
-        }
-        self.proxies['DIRECT'] = 'DIRECT'
-        self.routes = data['routes']
-
-        for route in self.routes.values():
-            if route not in self.proxies:
-                print('ERROR: `{}` is not in the proxies list'.format(route))
-                sys.exit(1)
-
-        self.default_proxy = data['default_proxy']
-        if self.default_proxy not in self.proxies:
-            print('ERROR: `{}` is not in the proxies list'.format(
-                self.default_proxy
-            ))
-            sys.exit(1)
-        self.default_proxy = self.proxies[self.default_proxy]
-
-        excludes = data.get('excludes', [])
-        for route in excludes:
-            self.routes[route] = 'DIRECT'
-
-    @staticmethod
-    def parse_proxy(proxy_url):
-        proxy_url = proxy_url.strip()
-        parsed_url = urlparse(proxy_url)
-        scheme = {
-            'http': 'HTTP',
-            'https': 'HTTPS',
-            'socks': 'SOCKS',
-            'socks4': 'SOCKS4',
-            'socks5': 'SOCKS5'
-        }.get(parsed_url.scheme.lower(), 'SOCKS5')
-        return '{} {}'.format(
-            scheme,
-            parsed_url.netloc
-        )
-
-    def generate_pac(self, output_path=None):
-        pac = """
+pac_template = """
 var proxies = {proxies};
 
 var routes = {routes};
@@ -74,12 +29,54 @@ function FindProxyForURL(url, host) {{
         }}
         pos = host.lastIndexOf('.', pos - 1);
     }};
-}};
-        """.format(
+}};"""
+
+
+class PacGen:
+    ALLOWED_SCHEMES = ['HTTP', 'HTTPS', 'SOCKS', 'SOCKS4', 'SOCKS5']
+
+    def __init__(self, config_path):
+        with open(config_path, 'r') as f:
+            data = yaml.load(f, Loader=yaml.FullLoader)
+
+        self.proxies = {
+            name: self.parse_proxy(addr)
+            for name, addr in data['proxies'].items()
+        }
+        self.proxies['DIRECT'] = 'DIRECT'
+
+        self.routes = data['routes']
+        for route in self.routes.values():
+            if route not in self.proxies:
+                print('ERROR: `{}` is not in the proxies list'.format(route))
+                sys.exit(1)
+
+        try:
+            self.default_proxy = self.proxies[data['default_proxy']]
+        except KeyError:
+            print('ERROR: `{}` is not in the proxies list'.format(
+                data['default_proxy']
+            ))
+            sys.exit(1)
+
+        for route in data.get('excludes', []):
+            self.routes[route] = 'DIRECT'
+
+    @staticmethod
+    def parse_proxy(proxy_url):
+        proxy_url = proxy_url.strip()
+        parsed_url = urlparse(proxy_url)
+        scheme = parsed_url.scheme.upper()
+        if scheme not in PacGen.ALLOWED_SCHEMES:
+            print('ERROR: Unknown scheme `{}`'.format(scheme))
+            sys.exit(1)
+        return '{} {}'.format(scheme, parsed_url.netloc)
+
+    def generate_pac(self, output_path):
+        pac = pac_template.format(
             proxies=pprint.pformat(self.proxies),
             routes=pprint.pformat(self.routes),
             default_proxy=self.default_proxy
         )
-
         with open(output_path, 'w') as f:
             f.write(pac)
